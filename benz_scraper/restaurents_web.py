815201b25e8dca21a349e9d3455b99d184831ef4
Soufiane Benzaoui <soubenz94@gmail.com>
2024-01-28 01:02:39 +0100

    Working version of review scraper

--- benz_scraper/restaurents_web.py
+++ benz_scraper/restaurents_web.py
@@ -5,35 +5,37 @@ import time
 from pydantic import BaseModel
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
 from loguru import logger
+from playwright_stealth import stealth_sync
+from benz_scraper.scrapers.reviews_scraper import ReviewsScraper
+from benz_scraper.utils import debug_delay, dump_items_to_json, parse_date, slight_delay
+from datetime import datetime, timedelta
+import dateparser
 
 
-def debug_delay():
-    time.sleep(1000)
-
-
-class RestaurantReview(BaseModel):
-    id: int
-    text: str = None
-    stars: str
-    date: str
 
 class Scrape_Google:
     
     def __init__(self, playwright):
-        self.browser = playwright.chromium.launch(headless=False)
+        self.browser = playwright.chromium.launch(headless=True)
         context = self.browser.new_context()
-        self.page = context.new_page()
+        page = context.new_page()
+        stealth_sync(page)
+        self.page = page
+        logger.info("browser ready")
         
     def consent_cookies(self):
         self.page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
         button = self.page.locator('//form').nth(1)
         button.click()
         time.sleep(2)
+        logger.info("consent given")
 
 
     def navigate(self, url):
         #  "https://www.google.com/maps/place/Nabou+Pastel+Paris+19%C3%A8me/@48.8942894,2.3783428,17z/data=!4m6!3m5!1s0x47e66d3c5de986f9:0xac4b79771d885c92!8m2!3d48.8939687!4d2.3814685!16s%2Fg%2F11h88czw4_?hl=en&entry=ttu"
         self.page.goto(url)
+        slight_delay()
+        logger.info("Page is loaded")
         
     def prepare_reviews(self):
              
@@ -42,49 +44,69 @@ class Scrape_Google:
         button = self.page.locator('//button[@data-value="Sort"]').click()
         time.sleep(2)
         menu = self.page.locator('div#action-menu')
-        # debug_delay()
+
         menu.locator('//div[@role="menuitemradio"][@data-index="1"]').click()
+        self.reviews = self.page.locator('div.jftiEf')
         
     def get_metadata(self):
         
-        self.reviews = self.page.locator('div.jftiEf')
-        nb_reviews = self.page.locator('div.jANrlb > div.fontBodySmall').inner_text()
-        self.true_nb = int(nb_reviews.split(" reviews")[0])
-        # print(true_nb)
-        # pages = floor(true_nb / 10)
+        # nb_reviews = self.page.locator('div.jANrlb > div.fontBodySmall').inner_text()
+        nb_reviews = self.page.locator('div.F7nice > span:nth-child(2) > span > span').inner_text()
+        av_stars = self.page.locator('span.ceNzKf').get_attribute("aria-label")
+        title = self.page.locator('h1.DUwDvf > span.a5H0ec').inner_text()
+        category = self.page.locator('button.DkEaL ').inner_text()
+        self.true_nb = int(nb_reviews.strip("()"))
+        slight_delay()
+        self.metadata = RestaurantMetadata(category=category, name=title, nb_reviews=nb_reviews, average_rating=av_stars)
+        print(category, title, nb_reviews, av_stars, self.true_nb)
         
         
     def scrape_review(self, review_idx):
         result_elem = self.reviews.nth(review_idx)
         try:
+            button_more = result_elem.locator('div.MyEned > span > button.w8nwRe').click(timeout=1500)
+        except PlaywrightTimeoutError:
+            pass
+        try:
             text = result_elem.locator('//div[@class="MyEned"]/span[@class="wiI7pd"]').inner_text(timeout=1500)
         except PlaywrightTimeoutError:
+            logger.warning("no text in review")
             text= None
         stars = result_elem.locator('//span[@class="kvMYJc"]').get_attribute("aria-label")
-        date = result_elem.locator('//span[@class="rsqaWe"]').inner_text()
-        item = RestaurantReview(id=review_idx + 1, text=text, stars=stars, date=date)
+        date_text = result_elem.locator('//span[@class="rsqaWe"]').inner_text()
+        reviewer = result_elem.locator('div.d4r55 ').inner_text()
+        try:
+            reviewer_stats = result_elem.locator('div.RfnDt ').inner_text(timeout=1500)
+        except PlaywrightTimeoutError:
+            reviewer_stats = None
+            logger.warning("no reviewer stats")
+    
+        extra_data = result_elem.locator('div.PBK6be').all_inner_texts()
+        print(reviewer, reviewer_stats, extra_data)
+        date = dateparser.parse(date_text)
+        slight_delay()
+        item = RestaurantReview(id=review_idx + 1, text=text, stars=self.clean_stars(stars), date=str(date))
         print(item)
         return item
+
+   
+    
+    def clean_stars(self,stars_text):
+        return stars_text.split(" stars")[0]
     
     def close(self):
         self.browser.close()
-    
+
 def scrape_restaurants():
     with sync_playwright() as playwright:
-        b = Scrape_Google(playwright)
+        b = ReviewsScraper(playwright)
         b.navigate("https://www.google.com/maps/place/Nabou+Pastel+Paris+19%C3%A8me/@48.8942894,2.3783428,17z/data=!4m6!3m5!1s0x47e66d3c5de986f9:0xac4b79771d885c92!8m2!3d48.8939687!4d2.3814685!16s%2Fg%2F11h88czw4_?hl=en&entry=ttu")
         
-        # page.set_viewport_size(width=375, height=812)
-
-        # button = page.query_selector("form:nth-child(1) > div > div > button")
-        # button.click()
-        # time.sleep(70)
-        b.consent_cookies()
-
-        
-        b.prepare_reviews()
+        b.consent_cookies()   
         b.get_metadata()
-        
+        b.prepare_reviews()
+        # debug_delay()
+        items = []
         print(b.true_nb)
         scrapped_item = 1
         while scrapped_item < b.true_nb:
@@ -93,6 +115,7 @@ def scrape_restaurants():
             for _ in range(10):
                 item = b.scrape_review(scrapped_item)
                 print(item)
+                items.append(item)
                 # result_elem = reviews.nth(scrapped_item)
                 # try:
                 #     text = result_elem.locator('//div[@class="MyEned"]/span[@class="wiI7pd"]').inner_text(timeout=1500)
@@ -102,30 +125,9 @@ def scrape_restaurants():
                 # stars = result_elem.locator('//span[@class="kvMYJc"]').get_attribute("aria-label")
                 # date = result_elem.locator('//span[@class="rsqaWe"]').inner_text()
                 # item = RestaurantReview(id=scrapped_item + 1, text=text, stars=stars, date=date)
-                # print(item)
-                scrapped_item+= 1
-        
-        
-        # time.sleep(170)
-        # reviews = page.locator('//*[@id="app"]/div[12]/div[2]/div/div[2]/div[3]/div/div[3]/div/div[3]/span').inner_text()
-        # return
-        # data = []
-        # for k in reviews:
-        # page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
-        
-        
-        # for i in range(1):
-        #     page.mouse.wheel(0, 100000)
-        #     time.sleep(2)
-        #     #app > div.ml-pane-container > div.visible > div > div.nkePVe > div:nth-child(3) > div:nth-child(9) > div.HQtAOc > div > div.QGH3wd.Inlyae > div > span
-            
-        # #     data.append(k.text_content())page.on("request", lambda request: print(">>", request.method, request.url))
-        #     is_data = False
-        #     # with page.expect_response(re.compile(r"lh3.googleusercontent.+")) as response_info:
-        #     #     is_data = True
-        #     # print(is_data)
-            
-        #     # page.on("response", lambda response: print("<<", response.status, response.url))
-        
 
+                scrapped_item+= 1
+                
+        dump_items_to_json(items, "nabou")
+        
 scrape_restaurants()
